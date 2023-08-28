a8faa2827b4063e5778dcec35bf317b3af0156b4
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2023-08-28 00:55:15 -0300

    Initial structure

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,12 +2,12 @@
 
 describe('Login', () => {
 
-    beforeEach(() => {
-      cy.visit('https://www.saucedemo.com/')
-    })
-
-    it('Login successfully', () => {
-        cy.login('standard_user', 'secret_sauce')
-        cy.get('.app_logo').should('have.text', 'Swag Labs')
-    })
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
   })
+
+  it('Login successfully', () => {
+    cy.login('standard_user', 'secret_sauce')
+    cy.get('.app_logo').should('have.text', 'Swag Labs')
+  })
+})
