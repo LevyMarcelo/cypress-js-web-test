99037881cd114a78ae65262cf045299e6a270dc4
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2023-08-27 23:49:19 -0300

    Initial structure

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Login', () => {
+
+    beforeEach(() => {
+      cy.visit('https://www.saucedemo.com/')
+    })
+
+    it('Login successfully', () => {
+        cy.login('standard_user', 'secret_sauce')
+        cy.get('.app_logo').should('have.text', 'Swag Labs')
+    })
+  })
