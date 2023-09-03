9e4541873cf484c129fdd0b7ea08901a60b85092
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2023-09-03 20:10:35 -0300

    Login failed

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -8,6 +8,34 @@ describe('Login', () => {
 
   it('Login successfully', () => {
     cy.login('standard_user', 'secret_sauce')
-    cy.get('.app_logo').should('have.text', 'Swag Labs')
+    cy.get('.title').should('have.text', 'Products')
+    cy.url().should('eq', 'https://www.saucedemo.com/inventory.html') 
+  })
+
+  it('Login failed - empty username and password', () => {
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required')
+  })
+
+  it('Login failed - empty username', () => {
+    cy.login('{ESC}', 'secret_sauce')
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required')
+  })
+
+  it('Login failed - empty password', () => {
+    cy.login('standard_user', '{ESC}')
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required')
+  })
+
+  it('Login failed - invalid user', () => {
+    cy.login('teste', 'secret_sauce')
+    cy.get('[data-test="error"]')
+      .should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+  })
+
+  it('Login failed - invalid password', () => {
+    cy.login('standard_user', 'teste')
+    cy.get('[data-test="error"]')
+      .should('have.text', 'Epic sadface: Username and password do not match any user in this service')
   })
 })
